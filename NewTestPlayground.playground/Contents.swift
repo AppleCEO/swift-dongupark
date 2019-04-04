54703661004bf0c8a7952e9e60d329d45909fc75
kiljh <kiljh@me.com>
2019-04-04 23:30:58 +0900

    Lecture7 Generic

--- NewTestPlayground.playground/Contents.swift
+++ NewTestPlayground.playground/Contents.swift
@@ -274,3 +274,63 @@ func printMathResult(mathFunction: (Int, Int) -> Int, a: Int, b: Int){
     print("Result: \(mathFunction(a, b))")
 }
 printMathResult(mathFunction: addTwoInts, a: 3, b: 5)
+
+
+// 7강 제너릭
+//
+/// 제너릭스를 이용한 swap 함수 - 다양한 자료형에 대한 swap이 가능
+func swapTwoValues<T>( a: inout T, b: inout T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+// 하나의 함수로 여러 타입의 데이터 처리가 가능하다
+n1 = 100; n2 = 200
+swapTwoValues(a: &n1, b: &n2)
+n1
+n2
+
+var s1 = "Hello"; var s2 = "World"
+swapTwoValues(a: &s1, b: &s2)
+s1
+s2
+
+func repeatInt(_ item: Int, times: Int) -> [Int]
+{
+    var result = [Int]()
+    for _ in 1...times {
+        result.append(item)
+    }
+    
+    return result
+}
+
+repeatInt(3, times: 5)
+repeatInt(4, times: 2)
+
+func repeatString(_ item: String, times: Int) -> [String]
+{
+    var result = [String]()
+    for _ in 1...times {
+        result.append(item)
+    }
+    
+    return result
+}
+
+repeatString("Hi", times: 5)
+repeatString("Hello", times: 3)
+
+func repeatItem<T>(_ item: T, times: Int) -> [T] {
+    var result = [T]()
+    for _ in 1...times {
+        result.append(item)
+    }
+    
+    return result
+}
+
+repeatItem("knock", times: 4)
+repeatItem(10, times: 4)
+repeatItem(1.1, times: 3)
