3f4078fb299b1bb2fe26bd1b8fda91f3da381347
kiljh <kiljh@me.com>
2019-04-06 20:24:58 +0900

    Lecture 11 value type, reference type

--- NewTestPlayground.playground/Contents.swift
+++ NewTestPlayground.playground/Contents.swift
@@ -471,3 +471,60 @@ let hd = Resolution(width: 1920, height: 1080)
 var cinema = hd
 cinema.width = 2048
 //hd.width = 2048
+
+
+// 11강 값타입과 참조타입
+
+struct PersonInfo {
+    // struct의 속성(property)이라고 함
+    var SSN : Int   // 변수 속성
+    let age : Int   // 상수 속성
+}
+
+var person1 = PersonInfo(SSN: 111222, age: 23)
+person1.SSN = 112233
+person1
+//person1.age = 10
+
+// 상수 구조체의 저장속성은 변경이 불가능 - 아래 주석문은 error
+let person2 = PersonInfo(SSN: 4, age: 29)
+//person2.SSN = 222233
+
+struct Round {
+    var radius : Double = 0.0
+    var area : Double = 0.0
+}
+
+var r = Round()
+var copy_r = r
+copy_r.radius = 10.0
+copy_r
+
+class Circle {
+    var radius : Double = 0.0
+    var area : Double = 0.0
+}
+
+// c는 Circle 인스턴스를 참조함
+var c = Circle ()
+// copy_c는 Circle 인스턴스 참조 변수임
+var copy_c : Circle
+// copy_c가 c가 참조하는 Circle 인스턴스를 참조함
+copy_c = c
+copy_c.radius = 10
+copy_c
+c
+
+class OtherReference {
+    var a = 10
+}
+
+// 변수형 클래스의 속성은 당연히 변경 가능함
+var ref3 = OtherReference()
+ref3.a = 100
+// 상수형 클래스의 속성 역시 변경가능함
+let ref4 = OtherReference()
+ref4.a = 100
+
+//ref4 = ref3
+ref3 = ref4
